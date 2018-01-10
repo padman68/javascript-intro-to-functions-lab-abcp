f2ac2edc669b9c59ad204c4860d5f9dd2e32d53c
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-10 23:43:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string.toLowerCase() === string
+  string.toLowerCase() == string
   return sayHiToGrandma("I can't hear you!")
 }
 
