d9dbdfd70318a4da420981dd157210525ba94b56
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-10 23:22:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var lowercase = 'I can\'t hear you!'
- 
 lowercase.toLowerCase() === string
   return sayHiToGrandma('I can\'t hear you!')
 }
