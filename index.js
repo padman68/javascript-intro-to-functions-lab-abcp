9f5b73862f0adfa31da3cc593701104aa3dea3e6
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-11 00:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
 }
 else if (string === string.toUpperCase()) {
     return "YES INDEED!";
-} else if ('I love you, Grandma.') {
+} else if (string == 'I love you, Grandma.') {
   return 'I love you, too.'
 }
