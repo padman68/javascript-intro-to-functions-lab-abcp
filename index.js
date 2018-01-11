8fa7f359d8903eb1a4805aa65afe9368d6085229
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-11 00:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ function logWhisper(string) {
   console.log (string.toLowerCase())
 }
 
-else if (string == 'I love you, Grandma.') {
-  return 'I love you, too.'
-}
-
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
@@ -25,5 +21,9 @@ function sayHiToGrandma(string) {
 else if (string === string.toUpperCase()) {
     return "YES INDEED!";
 } else if ('I love you, Grandma.') {
+  return 'I love you, too.'
+}
+
+else if (string == 'I love you, Grandma.') {
   return 'I love you, too.'
 }
